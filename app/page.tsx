0752c208a8e5e193f3368cff34a69a7b7fcaf771
shejoycn <shejoycn@gmail.com>
2024-07-26 13:58:35 +0400

    env

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ export default function Home() {
           <p>
               1 <img src="https://opdc01mstry4a91inte.dxcloud.episerver.net/globalassets/vision-and-mission/vision.png" />
               2 <img src="/globalassets/vision-and-mission/vision.png" />          </p>
+
+              <b>
+                {process.env.OPTIMIZELY_BASE_URL}
+              </b>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
